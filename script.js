a3a3af692dbdac108b45f4711136900d5ee07d19
Pankaj Dani <pankajdani511@gmail.com>
2026-02-23 21:53:05 +0530

    apply all image filters using canvas context

--- script.js
+++ script.js
@@ -1,91 +1,141 @@
-const filters={
-    brightness:{
-        value:100,  
-        min:0,
-        max:200,
-        unit:"%",
+const filters = {
+    brightness: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%",
     },
-    contrast:{
-        value:100,  
-        min:0,
-        max:200,
-         unit:"%",
+    contrast: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%",
     },
-    exposure:{
-        value:100,  
-        min:0,
-        max:200,
-         unit:"%",
+    saturation: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%",
     },
-    saturation:{
-        value:100,  
-        min:0,
-        max:200,
-         unit:"%",
+    hueRotate: {
+        value: 0,
+        min: 0,
+        max: 360,
+        unit: "deg",
     },
-    hueRotate:{
-        value:100,  
-        min:0,
-        max:360,
-       unit:"deg",
+    blur: {
+        value: 0,
+        min: 0,
+        max: 20,
+        unit: "px",
     },
-    blur:{
-        value:100,  
-        min:0,
-        max:20,
-        unit:"px",
+    grayscale: {
+        value: 0,
+        min: 0,
+        max: 100,
+        unit: "%",
     },
-    grayscale:{
-        value:0,
-        min:0,
-        max:100,
-        unit:"%",
+    sepia: {
+        value: 0,
+        min: 0,
+        max: 100,
+        unit: "%",
     },
-    sepia:{
-        value:0,
-        min:0,
-        max:100,
-        unit:"%",
+    opacity: {
+        value: 100,
+        min: 0,
+        max: 100,
+        unit: "%",
     },
-    opacity:{
-        value:100,  
-        min:0,
-        max:100,
-        unit:"%",
-    },
-    invert:{
-        value:0,
-        min:0,
-        max:100,
-        unit:"%",
+    invert: {
+        value: 0,
+        min: 0,
+        max: 100,
+        unit: "%",
     },
 }
 
-const filtersContainer=document.querySelector(".filters");
+const imageCanvas = document.querySelector("#image-canvas");
+const imginput = document.querySelector("#image-input");
+const canvasCtx = imageCanvas.getContext("2d");
+let file = null;
+let image = null;
 
-function createFilterElement(name,unit="%",value,min,max){
- const div=document.createElement("div");
- div.classList.add("filter");
 
- const input=document.createElement("input");
-    input.type="range";
-    input.id=name;
-    input.value=value;
-    input.min=min;
-    input.max=max;
 
-    const p=document.createElement("p");
-    p.textContent=name;
+const filtersContainer = document.querySelector(".filters");
+
+function createFilterElement(name, unit = "%", value, min, max) {
+    const div = document.createElement("div");
+    div.classList.add("filter");
+
+    const input = document.createElement("input");
+    input.type = "range";
+    input.id = name;
+    input.value = value;
+    input.min = min;
+    input.max = max;
+
+    const p = document.createElement("p");
+    p.textContent = name;
     div.appendChild(p);
     div.appendChild(input);
+
+    input.addEventListener("input", (e) => {
+        filters[name].value = input.value;
+        applyFilters();
+    });
+
 
     return div;
 }
 
-Object.keys(filters).forEach(key=>{ 
+Object.keys(filters).forEach(key => {
 
-    const filterElement= createFilterElement(key,filters[key].unit,filters[key].value,filters[key].min,filters[key].max);
+    const filterElement = createFilterElement(key, filters[key].unit, filters[key].value, filters[key].min, filters[key].max);
     filtersContainer.appendChild(filterElement);
 
 
 })
+
+
+
+imginput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    const imageplaceholder = document.querySelector(".placeholder");
+    imageCanvas.style.display = "block";
+    imageplaceholder.style.display = "none";
+
+
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+
+    img.onload = () => {
+
+        image = img; 
+
+        imageCanvas.width = img.width;
+        imageCanvas.height = img.height;
+
+        canvasCtx.drawImage(img, 0, 0, imageCanvas.width, imageCanvas.height);
+    }
+});
+
+function applyFilters(){
+
+    canvasCtx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+    
+    canvasCtx.filter = `
+        brightness(${filters.brightness.value}${filters.brightness.unit})
+        contrast(${filters.contrast.value}${filters.contrast.unit})
+        saturate(${filters.saturation.value}${filters.saturation.unit})
+        hue-rotate(${filters.hueRotate.value}${filters.hueRotate.unit})
+        blur(${filters.blur.value}${filters.blur.unit})
+        grayscale(${filters.grayscale.value}${filters.grayscale.unit})
+        sepia(${filters.sepia.value}${filters.sepia.unit})
+        opacity(${filters.opacity.value}${filters.opacity.unit})
+        invert(${filters.invert.value}${filters.invert.unit})
+        `.trim();
+
+    canvasCtx.drawImage(image,0,0);
+}
